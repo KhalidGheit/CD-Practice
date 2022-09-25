c83aef725ab09be38e378057ce24db1e753c1647
Khalid Aboulgheit <khalidaboulgheit1@gmail.com>
2022-09-25 22:40:27 +0200

    1

--- roles/setup/files/index.js
+++ roles/setup/files/index.js
@@ -2,6 +2,7 @@ var http = require("http");
 var server = http.createServer(function (req, res) {
 res.writeHead(200);
 res.end("Hello world!");
+console.log("Hello world!");
 });
 server.listen(3000);
 
